92cc1414874911a1dac0ecd25e1851be4f1b9e2b
Imaad Daniels <imaad.daniels1@gmail.com>
2026-09-23 22:00:47 +0200

    feat : added user prompt dialogue for setup wizard, added helper methods to store credentials in .env

--- setup_wizard.py
+++ setup_wizard.py
@@ -1,24 +1,55 @@
-# import encryption.cipher
+import os
+import re
+from encryption import cipher
+from dotenv import load_dotenv, set_key
 
-def getAppPassword() :
-    message = "Enter App Password :"
-    print(promptInput(message))
-    
-def getApi() : 
-    message = "Enter API URL :"
-    print(promptInput(message))
-    
-def getEmail() :
-    message = "Enter Phishing email :"
-    print(promptInput(message))
-    
-def main():
-    getAppPassword()
-    getApi()
-    getEmail()    
-    
+
 def promptInput(promptMessage : str = "") -> str :
     return input(promptMessage)
+
+def is_valid_email(email : str) -> bool :
+    EMAIL_REGEX = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    
+    return bool(re.match(EMAIL_REGEX, email))
+        
+def add_to_env(key : str, value : str) :
+    dotenv_path = ".env"
+
+    if not os.path.exists(dotenv_path):
+        open(dotenv_path, "w").close()
+
+    set_key(dotenv_path, key, value)
+
+    load_dotenv(dotenv_path, override=True)
+
+
+def main():
+    print("""
+==================================================
+        WELCOME TO THE DEEPCAST SETUP WIZARD
+==================================================
+
+To ensure DeepCast functions properly, you need to configure your
+email credentials. These details allow the application to send emails
+as intended.
+
+Please follow the instructions below and make sure your credentials 
+are entered correctly. If you enter incorrect information or experience
+a failure, you can update your settings at any time.
+""")  
+    
+app_password = input("""
+An App Paswword is required for the yagmail and smtplib methods. Which also
+requires 2FA(two factor authentication) to be enabled. If that has not been
+established it is adviced for the functionality of this application to do so
+
+Enter App Password :
+""")
+
+api_url = input("""
+An API url is required to use the sendgrid method
+""")
+
     
 if __name__ == "__main__":
     main()
